fbddd867e6ac617bc0c57a39b77a5e8318c2f210
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-02-13 19:00:35 -0300

    Ajuste na responsividade do Site e remoção do Dropdown
    A princípio, decidi remover o dropdown do site de apresentação do Korivo, pois, por se tratar de um sistema novo, suas funcionalidades ainda são básicas. Neste momento, não há necessidade de direcionar para múltiplas páginas por meio de um menu dropdown.

--- src/layout/Header/components/Navgation/Navigation.jsx
+++ src/layout/Header/components/Navgation/Navigation.jsx
@@ -1,7 +1,6 @@
 'use client'
 import Link from "next/link";
 import styles from "./Navigation.module.css";
-import { IoIosArrowDown } from "react-icons/io";
 
 export default function Navigation({ isOpen, onClose }) {
   const dt_links = [
@@ -12,22 +11,10 @@ export default function Navigation({ isOpen, onClose }) {
     {
       label: "Soluções",
       href: "/#solutions",
-      dropdown: [
-        { label: "Dashboards Personalizados", href: "/service" },
-        { label: "Cadastro de Clientes", href: "/service" },
-        { label: "Estoque em tempo real", href: "/service" },
-        { label: "Contas a pagar e a receber", href: "/service" },
-        { label: "Extrato e integração com o Banco", href: "/service" },
-      ],
     },
     {
       label: "Base",
       href: "/#cards-services",
-      dropdown: [
-        { label: "Next.js", href: "/tech" },
-        { label: "WoordPress", href: "/tech" },
-        { label: "Sistemas de Gestão", href: "/tech" },
-      ],
     },
     {
       label: "Valores",
@@ -49,42 +36,15 @@ export default function Navigation({ isOpen, onClose }) {
         {dt_links.map((link, index) => (
           <li
             key={index}
-            className={`${styles.navItem} ${link.dropdown ? styles.hasDropdown : ""}`}
+            className={styles.navItem}
           >
-            {link.dropdown ? (
-              <div className={styles.dropdownWrapper}>
-                <Link
-                  className={`${styles.link} ${styles.dropdownLink}`}
-                  href={link.href}
-                  onClick={() => handleLinkClick()}
-                >
-                  {link.label}
-                  <IoIosArrowDown className={styles.arrowIcon} />
-                </Link>
-
-                <ul className={styles.dropdownMenu}>
-                  {link.dropdown.map((item, itemIndex) => (
-                    <li key={itemIndex}>
-                      <Link
-                        className={styles.dropdownItem}
-                        href={item.href}
-                        onClick={() => handleLinkClick()}
-                      >
-                        {item.label}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : (
-              <Link
-                className={styles.link}
-                href={link.href}
-                onClick={() => handleLinkClick()}
-              >
-                {link.label}
-              </Link>
-            )}
+            <Link
+              className={styles.link}
+              href={link.href}
+              onClick={handleLinkClick}
+            >
+              {link.label}
+            </Link>
           </li>
         ))}
       </ul>
